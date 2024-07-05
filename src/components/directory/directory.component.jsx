654b4418b97baa6a9c41cea2229c227b7796517e
Riwalenn Bas <riwalenn@gmail.com>
2024-07-05 22:49:46 +0200

    [MAJ] fixing clashing styles

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,4 @@
-import CategoryItem from "../category-item/category-item.component";
+import DirectoryItem from "../directory-item/directory-item.component";
 import './directory.styles.scss';
 const Directory = () => {
     const categories = [
@@ -32,7 +32,7 @@ const Directory = () => {
     return (
         <div className="directory-container">
             { categories.map((category) => (
-                <CategoryItem category={category } key={category.id} />
+                <DirectoryItem category={category } key={category.id} />
             ))}
         </div>
     );
