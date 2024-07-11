aa2faf29645348f5b06931e7f46cd1dc7c9b2eeb
Riwalenn Bas <riwalenn@gmail.com>
2024-07-11 18:33:36 +0200

    [MAJ] redux-devTools

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,11 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
+import logger from "redux-logger";
 
 import { rootReducer } from './root-reducer';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
 
-  next(action);
-};
 
 const persistConfig = {
   key: 'root',
@@ -20,9 +15,11 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
