1b986a7c6ec4fae6c6c69ceae4325ad70da88e67
Riwalenn Bas <riwalenn@gmail.com>
2024-07-11 18:11:08 +0200

    [MAJ] migrate cart context to redux

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,18 @@
 import { CartItemContainer, ItemDetails } from './cart-item.styles';
 
 const CartItem = ({ cartItem }) => {
-    const { name, imageUrl, price, quantity } = cartItem;
-    return (
-        <CartItemContainer>
-            <img src={imageUrl} alt={`${name}`} />
-            <ItemDetails>
-                <span>{name}</span>
-                <span>
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <CartItemContainer>
+      <img src={imageUrl} alt={`${name}`} />
+      <ItemDetails>
+        <span>{name}</span>
+        <span>
           {quantity} x ${price}
         </span>
-            </ItemDetails>
-        </CartItemContainer>
-    );
+      </ItemDetails>
+    </CartItemContainer>
+  );
 };
 
 export default CartItem;
